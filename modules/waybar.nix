48d17d2897a7885a03b7262d2a7b58be5e73437c
libewa <libewa.git@icloud.com>
2024-06-23 01:21:13 +0200

    add left waybar

--- modules/waybar.nix
+++ modules/waybar.nix
@@ -3,11 +3,34 @@
     enable = true;
     systemd.enable = true;
     settings = {
+      leftbar = {
+        height = 50;
+	spacing = 20;
+	position = "left";
+	modules-left = [ "custom/power" ];
+	modules-center = [ "hyprland/workspaces" ];
+	modules-right = [ "tray" ];
+
+        tray = {
+          spacing = 10;
+        };
+        "custom/power" = {
+          format = "󰐥 Power options";
+          on-click = "rofi -show power-menu -modi power-menu:rofi-power-menu";
+        };
+	"hyprland/workspaces" = {
+	  format-icons = {
+	    empty = "";
+	    active = "";
+	    default = "";
+	  };
+	  format = "{icon}";
+	};
+      };
       topbar = {
         height = 30;
         spacing = 10;
         position = "top";
-        modules-left = [ "custom/power" "tray" ];
         modules-center = [ "hyprland/window" ];
         modules-right = [ "network" "pulseaudio" "memory" "backlight" "keyboard-state" "hyprland/language" "battery" "clock" ];
 
@@ -19,21 +42,14 @@
             locked = "";
             unlocked = "";
           };
-        };
-        tray = {
-          spacing = 10;
-        };
+        }; 
         clock = {
-          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-          format-alt = "{:%d.%m.%Y}";
+          tooltip-format = "{:%d.%m.%Y}";
         };
         memory = {
           format = "{}% ";
-        };
-        "custom/power" = {
-          format = "󰐥 Power options";
-          on-click = "rofi -show power-menu -modi power-menu:rofi-power-menu";
-        };
+	  onclick = "kitty htop";
+        }; 
         backlight = {
           format = "{percent}% {icon}";
           format-icons = [ "" "" "" "" "" "" "" "" "" ];
@@ -55,10 +71,10 @@
         network = {
           format-wifi = "{essid} ({signalStrength}%) ";
           format-ethernet = "{ipaddr}/{cidr} ";
-          tooltip-format = "{ifname} via {gwaddr} ";
           format-linked = "{ifname} (No IP) ";
           format-disconnected = "Disconnected ⚠";
-          format-alt = "{ifname}: {ipaddr}/{cidr}";
+          tooltip-format = "{ifname}: {ipaddr}/{cidr}";
+	  on-click = "networkmanager_dmenu";
         };
 
         pulseaudio = {
@@ -77,33 +93,40 @@
       };
     };
     style = ''
-    * {
-      font-family: "Ubuntu Nerd Font";
-      font-size: 13px;
-    }
-    window#waybar {
-      border: none;
-      border-radius: 0; 
-      min-height: 0;
-      background: none;
-      color: #fff;
-      padding-top: 2px;
-      padding-bottom: 2px
-    }
+      * {
+	font-family: "Ubuntu Nerd Font";
+	font-size: 13px;
+      }
+      /*#window {
+	max-width = 350px;
+      }*/
+      window#waybar {
+	border: none;
+	border-radius: 0; 
+	min-height: 0;
+	background: none;
+	color: #fff;
+	padding-top: 2px;
+	padding-bottom: 2px
+      }
 
-    window#waybar {
-      /* From https://css.glass */
-      background: rgba(255, 255, 255, 0.1);
-      box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-      border-bottom: 3px solid rgba(100, 114, 125, 0.5);
-      padding-left: 4px;
-      padding-right: 4px;
-    }
+      window#waybar {
+	/* From https://css.glass */
+	background: rgba(255, 255, 255, 0.1);
+	box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+	border-bottom: 3px solid rgba(100, 114, 125, 0.5);
+	padding-left: 4px;
+	padding-right: 4px;
+      }
 
-    tooltip {
-      background: rgba(43, 48, 59, 0.9);
-      border: 1px solid rgba(100, 114, 125, 0.5);
-    }
+      tooltip {
+	background: rgba(43, 48, 59, 0.9);
+	border: 1px solid rgba(100, 114, 125, 0.5);
+      }
+      #workspaces button {
+        color: white;
+	background: none;
+      }
     '';
   };
 }
